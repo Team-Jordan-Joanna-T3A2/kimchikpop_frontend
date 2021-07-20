05552aeee681df0fb5ff6a48fe244fdd75095de2
arbpotatoes <arbpotatoes@gmail.com>
2021-07-20 18:21:29 +1000

    Started building nav components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import NavPublic from './components/NavPublic';
 import './App.css';
+
+const theme = createTheme({
+  primary: '#000000',
+  secondary: '#ffffff',
+});
 
 
 function App() {
   return (
     <div className="App">
+    <NavPublic />
     <h1>Kimchi & Kpop</h1>
     <p>Hello World!</p>
     </div>
