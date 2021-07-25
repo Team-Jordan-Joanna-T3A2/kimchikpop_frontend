d224c2077306bfe53a2ed9dca02740f272bf07c4
arbpotatoes <arbpotatoes@gmail.com>
2021-07-25 12:44:36 +1000

    Added axios

---
+++ src/components/TodayBookings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+const TodayBookings = () => {
+    return (
+        <Grid>
+            <h1>TODAY'S BOOKINGS</h1>
+            <Grid>
+                
+            </Grid>
+        </Grid>     
+    );
+}
+
+export default TodayBookings;
