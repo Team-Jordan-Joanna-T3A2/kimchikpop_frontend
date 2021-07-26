58bd0a57fa0b78968434b0be61c9e612e5fa1e18
Joanna-S-Lee <annaoj91@gmail.com>
2021-07-26 16:02:55 +1000

    added radio buttons and changed to datepicker instead of datetimepicker

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,43 +1,40 @@
 import React, { useState } from "react";
 import Nav from "../components/Nav";
-import DatePicker from 'react-date-picker'
+import DatePicker from "react-date-picker";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import { Radio, RadioGroup, FormControlLabel, FormLabel, FormControl } from "@material-ui/core";
 
-
-
-const useStyles = makeStyles(theme => ({
-    formField: {
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'left',
-        alignItems: 'center',
-    },
-    label: {
-        color: 'gray',
-        marginRight: 'auto'
-    },
-    submit: {
-        margin: '20px',
-        width: theme.buttonWidth
-    }
-}))
-
-
+const useStyles = makeStyles((theme) => ({
+  formField: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "left",
+    alignItems: "center",
+  },
+  label: {
+    color: "gray",
+    marginRight: "auto",
+  },
+  submit: {
+    margin: "20px",
+    width: theme.buttonWidth,
+  },
+}));
 
 const Book = () => {
-    const classes = useStyles();  
+  const classes = useStyles();
   const [value, onChange] = useState(new Date());
+  const [time, setTime] = useState('11:30am')
 
   return (
     <div>
       <Nav type="public" />
       <Grid className={classes.formField}>
-
-      <form className="booking-form">
-        <label>DATE & TIME :</label>
-        <DatePicker
+        <form className="booking-form">
+          <label>DATE :</label>
+          <DatePicker
             calendarAriaLabel="Toggle calendar"
             clearAriaLabel="Clear value"
             dayAriaLabel="Day"
@@ -46,34 +43,45 @@ const Book = () => {
             onChange={onChange}
             value={value}
             yearAriaLabel="Year"
-        />
+            format="dd-MM-yyyy"
+          />
 
-        <label>GUESTS :</label>
-        <input type="number" min="1" max="5"></input>
+          <FormControl className={classes.formField}>
+          <FormLabel>TIME :</FormLabel>
+          <RadioGroup value={time} onChange={(e) => setTime(e.target.value)}>
+              <FormControlLabel value="11:30am" control={<Radio />} label="11:30am" />
+              <FormControlLabel value="1:30pm" control={<Radio />} label="1:30pm" />
+              <FormControlLabel value="3:30pm" control={<Radio />} label="3:30pm" />
+              <FormControlLabel value="5:30pm" control={<Radio />} label="5:30pm" />
+              <FormControlLabel value="7:30pm" control={<Radio />} label="7:30pm" />            
+          </RadioGroup>
+          </FormControl>
 
-        <h2>YOUR DETAILS</h2>
-        <label>FIRST NAME :</label>
-        <input type="text" />
+          <label>GUESTS :</label>
+          <input type="number" min="1" max="5"></input>
 
-        <label>LAST NAME :</label>
-        <input type="text" />
+          <h2>YOUR DETAILS</h2>
+          <label>FIRST NAME :</label>
+          <input type="text" />
 
-        <label>EMAIL :</label>
-        <input type="email" />
+          <label>LAST NAME :</label>
+          <input type="text" />
 
-        <label>PHONE NUMBER :</label>
-        <input type="text" />
+          <label>EMAIL :</label>
+          <input type="email" />
 
-        <Button
-          variant="contained"
-          color="primary"
-          id="submit"
-          className={classes.submit}
-        >
-          SUBMIT BOOKING
-        </Button>
-        
-      </form>
+          <label>PHONE NUMBER :</label>
+          <input type="text" />
+
+          <Button
+            variant="contained"
+            color="primary"
+            id="submit"
+            className={classes.submit}
+          >
+            SUBMIT BOOKING
+          </Button>
+        </form>
       </Grid>
     </div>
   );
